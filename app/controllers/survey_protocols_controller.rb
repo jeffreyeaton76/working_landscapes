37346e8a778a02c0398e6098a14e0fa7a7825cc5
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-07-07 13:20:26 -0400

    registers Mime type for exporting Excel format

--- app/controllers/survey_protocols_controller.rb
+++ app/controllers/survey_protocols_controller.rb
@@ -6,7 +6,7 @@ class SurveyProtocolsController < ApplicationController
     @survey_protocols = SurveyProtocol.all
     respond_to do |format|
       format.html
-      format.csv { send_data @products.to_csv }
+      format.csv { send_data @survey_protocols.to_csv }
     end
   end
 
