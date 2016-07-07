9f5af2dc144788a333aba69ae548f3f6a13dcd8d
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-07-07 13:13:28 -0400

    adds to_csv method to protocol model

--- app/controllers/survey_protocols_controller.rb
+++ app/controllers/survey_protocols_controller.rb
@@ -4,6 +4,10 @@ class SurveyProtocolsController < ApplicationController
 
   def index
     @survey_protocols = SurveyProtocol.all
+    respond_to do |format|
+      format.html
+      format.csv { send_data @products.to_csv }
+    end
   end
 
   def new
--- app/models/survey_protocol.rb
+++ app/models/survey_protocol.rb
@@ -1,3 +1,12 @@
 class SurveyProtocol < ActiveRecord::Base
   has_many :observations, foreign_key: :protocol_id
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |protocol|
+        csv << protocol.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
